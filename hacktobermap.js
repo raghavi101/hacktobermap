c5d4295d1516a1c0923eba98f87bf29a9d4f509f
raghavi101 <56163096+raghavi101@users.noreply.github.com>
2019-10-04 19:52:22 +0530

    Made Changes
    Made desired changes to participate in hacktoberfest. Thanks for this repo

--- hacktobermap.js
+++ hacktobermap.js
@@ -1,7 +1,7 @@
 var hacktobermap = [
   {
-    name: "Abhijit Parida",
-    location: "Bhubaneswar",
+    name: "Raghavi Shirur",
+    location: "Indore",
     latitude: 20.296059,
     longitude: 85.824539
   },
